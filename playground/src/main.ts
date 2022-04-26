09163113bd0974415abcccca93d12b20e0401c37
唐道源 <tangdaoyuan@megvii.com>
2022-04-26 15:40:18 +0800

    test: test for vite-ssg

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,7 +1,6 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { ViteSSG } from 'vite-ssg'
 import routes from 'virtual:generated-pages'
-import Starport from 'vue-starport'
+import StarportPlugin from 'vue-starport'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
@@ -9,11 +8,13 @@ import './styles/main.css'
 import 'uno.css'
 import './index.css'
 
-const app = createApp(App)
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-})
-app.use(router)
-app.use(Starport({ keepAlive: true }))
-app.mount('#app')
+// `export const createApp` is required instead of the original `createApp(App).mount('#app')`
+export const createApp = ViteSSG(
+  App,
+  { routes, base: import.meta.env.BASE_URL },
+  (ctx) => {
+    ctx.app.use(StarportPlugin({ keepAlive: true }))
+    // install all modules under `modules/`
+    // Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.(ctx))
+  },
+)
